2b386fddfe5419b3ec793a9cb92379f90799a910
gangbeomsu <kbs21c1987@gmail.com>
2024-05-04 20:16:53 +0900

    이미지 추가

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,15 +1,18 @@
-import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(
-    this.color1,
-    this.color2, {
-    super.key,
-  });
+  // const GradientContainer(
+  //   this.color1,
+  //   this.color2, {
+  //   super.key,
+  // });
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
@@ -24,8 +27,11 @@ class GradientContainer extends StatelessWidget {
           colors: [color1, color2],
         ),
       ),
-      child: const Center(
-        child: StyledText("Hello, World!"),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-1.png',
+          width: 200,
+        ),
       ),
     );
   }
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -5,10 +5,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(
-          Color.fromARGB(255, 197, 163, 255),
-          Color.fromARGB(255, 255, 163, 197),
-        ),
+        body: GradientContainer.purple(),
       ),
     ),
   );
