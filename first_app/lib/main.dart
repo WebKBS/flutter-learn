dd96f3ac51d645e8c47ef3e22fd4c43ee9fdef9c
gangbeomsu <kbs21c1987@gmail.com>
2024-05-04 18:09:11 +0900

    위젯 분리 완료

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,24 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color.fromARGB(255, 197, 163, 255),
-                Color.fromARGB(255, 255, 163, 197),
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Text('Hello World'),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(255, 197, 163, 255),
+            Color.fromARGB(255, 255, 163, 197),
+          ],
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World',
+          style: TextStyle(
+              fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
 }
