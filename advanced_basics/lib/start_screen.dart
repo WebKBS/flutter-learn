279393b774692998dd22aed8b5b732701cb2ecb4
gangbeomsu <kbs21c1987@gmail.com>
2024-05-05 21:28:42 +0900

    icon과 opacity 적용방법

--- advanced_basics/lib/start_screen.dart
+++ advanced_basics/lib/start_screen.dart
@@ -6,20 +6,34 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(context) {
     return Center(
-        child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset(
-          'assets/images/quiz-logo.png',
-          width: 300,
-        ),
-        const SizedBox(height: 20),
-        const Text('Welcome to the Quiz!',
-            style: TextStyle(
-                fontSize: 24, color: Color.fromARGB(255, 255, 255, 255))),
-        const SizedBox(height: 30),
-        OutlinedButton(onPressed: () {}, child: const Text('Start Quiz'))
-      ],
-    ));
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Opacity(
+            opacity: 0.5,
+            child: Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 300,
+              color: const Color.fromARGB(255, 115, 255, 171),
+            ),
+          ),
+          const SizedBox(height: 80),
+          const Text('Welcome to the Quiz!',
+              style: TextStyle(
+                  fontSize: 24, color: Color.fromARGB(255, 255, 255, 255))),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+              icon: const Icon(Icons.arrow_right_alt),
+              label: const Text('Start Quiz',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Color.fromARGB(255, 255, 255, 255)))),
+        ],
+      ),
+    );
   }
 }
