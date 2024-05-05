94cd7942221a0002194f6af6f914c2b76b0fb35a
gangbeomsu <kbs21c1987@gmail.com>
2024-05-05 19:41:59 +0900

    기본 세팅

---
+++ advanced_basics/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:advanced_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+     const MaterialApp(
+      home:  Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
