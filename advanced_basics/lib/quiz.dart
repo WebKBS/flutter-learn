a53cdd3f2091d6ecef51d4a4da7b0d4390a55a76
gangbeomsu <kbs21c1987@gmail.com>
2024-05-05 21:42:08 +0900

    위젯 분리하고 Statefull 위젯 만들기

---
+++ advanced_basics/lib/quiz.dart
@@ -0,0 +1,30 @@
+import 'package:advanced_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 54, 44, 23),
+              Color.fromARGB(12, 32, 23, 12)
+            ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
