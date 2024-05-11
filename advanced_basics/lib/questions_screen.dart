56116de5794052f2f3c4b7e0ab3763f4a6a22b58
gangbeomsu <kbs21c1987@gmail.com>
2024-05-11 16:38:12 +0900

    폰트 설치 및 사용법

--- advanced_basics/lib/questions_screen.dart
+++ advanced_basics/lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:advanced_basics/answer_button.dart';
 import 'package:advanced_basics/data/questions.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -34,10 +35,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             children: [
               Text(
                 currentQuestion.text,
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: Color.fromARGB(255, 255, 255, 255),
-                ),
+                style: GoogleFonts.lato(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: const Color.fromARGB(255, 201, 233, 252)),
                 textAlign: TextAlign.center,
               ),
               const SizedBox(height: 30),
