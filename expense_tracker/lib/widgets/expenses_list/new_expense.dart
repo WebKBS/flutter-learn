75a6759eb819d1c2d9f766bb262d066c8fbff9f4
gangbeomsu <kbs21c1987@gmail.com>
2024-05-12 19:30:45 +0900

    날짜 선택 추가

--- expense_tracker/lib/widgets/expenses_list/new_expense.dart
+++ expense_tracker/lib/widgets/expenses_list/new_expense.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
@@ -12,17 +13,22 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
 
-  void _presentDatePicker() {
+  void _presentDatePicker() async {
     final now = DateTime.now();
     final firstDate = DateTime(now.year - 1, now.month, now.day);
 
-    showDatePicker(
+    final pickedDate = await showDatePicker(
       context: context,
       initialDate: now,
       firstDate: firstDate,
       lastDate: now,
     );
+
+    setState(() {
+      _selectedDate = pickedDate;
+    });
   }
 
   @override
@@ -63,10 +69,15 @@ class _NewExpenseState extends State<NewExpense> {
                     mainAxisAlignment: MainAxisAlignment.end,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      const Text('날짜'),
+                      Text(
+                        _selectedDate == null
+                            ? '날짜 선택'
+                            : formatter.format(_selectedDate!),
+                      ),
                       IconButton(
-                          onPressed: _presentDatePicker,
-                          icon: const Icon(Icons.calendar_month)),
+                        onPressed: _presentDatePicker,
+                        icon: const Icon(Icons.calendar_month),
+                      ),
                     ],
                   ),
                 ),
