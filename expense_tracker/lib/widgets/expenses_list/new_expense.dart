3ed7f0b6fcad6998a52c4500f91cff9ffaa07c1d
gangbeomsu <kbs21c1987@gmail.com>
2024-05-12 17:17:50 +0900

    닫기 버튼 추가

--- expense_tracker/lib/widgets/expenses_list/new_expense.dart
+++ expense_tracker/lib/widgets/expenses_list/new_expense.dart
@@ -11,10 +11,11 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
-
+  final _amountController = TextEditingController();
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -31,16 +32,31 @@ class _NewExpenseState extends State<NewExpense> {
                 label: Text("Title"),
               ),
             ),
+            TextField(
+              controller: _amountController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                prefixText: '\$',
+                label: Text("Amount"),
+              ),
+            ),
             Row(
               children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Cancel'),
+                ),
                 ElevatedButton(
                   onPressed: () {
                     print(_titleController.text);
+                    print(_amountController.text);
                   },
                   child: const Text('Save Expense'),
                 ),
               ],
-            )
+            ),
           ],
         ));
   }
