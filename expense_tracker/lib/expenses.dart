1fd339d12f7391afd342dd735cca36143780e4ff
gangbeomsu <kbs21c1987@gmail.com>
2024-05-12 14:29:58 +0900

    list 아이템 출력

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,16 +12,43 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Groceries',
+      amount: 50.0,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: 'Train ticket',
+      amount: 30.0,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+    Expense(
+      title: 'Dinner',
+      amount: 20.0,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: 'Laptop',
+      amount: 1000.0,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Expenses'),
-          Text('Expenses will be shown here'),
-        ],
-      ),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Expenses')),
+      body: Column(children: [
+        const Text('The expenses list:'),
+        Expanded(
+          child: ExpensesList(expenses: _registeredExpenses),
+        )
+      ]),
     );
   }
 }
