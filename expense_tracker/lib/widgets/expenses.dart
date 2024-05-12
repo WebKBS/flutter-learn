13d3aabfc024cb1ae789ef8cdb6e3e8d49040190
gangbeomsu <kbs21c1987@gmail.com>
2024-05-12 19:57:38 +0900

    데이터 추가 위젯 만들기

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -44,8 +44,14 @@ class _ExpensesState extends State<Expenses> {
     showModalBottomSheet(
         context: context,
         builder: (ctx) {
-          return const NewExpense();
+          return NewExpense(onAddExpense: _addExpense);
         });
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
   }
 
   @override
--- expense_tracker/lib/widgets/expenses_list/new_expense.dart
+++ expense_tracker/lib/widgets/expenses_list/new_expense.dart
@@ -2,7 +2,9 @@ import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
-  const NewExpense({super.key});
+  const NewExpense({super.key, required this.onAddExpense});
+
+  final void Function(Expense expense) onAddExpense;
 
   @override
   State<NewExpense> createState() {
@@ -57,6 +59,17 @@ class _NewExpenseState extends State<NewExpense> {
         },
       );
     }
+
+    widget.onAddExpense(
+      Expense(
+        title: _titleController.text,
+        amount: enteredAmount!,
+        date: _selectedDate!,
+        category: _selectedCategory!,
+      ),
+    );
+
+    Navigator.of(context).pop();
   }
 
   @override
