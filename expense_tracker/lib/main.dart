b3fc84cc00c148320138713332a0bdf1a2f8c3f4
gangbeomsu <kbs21c1987@gmail.com>
2024-05-15 22:09:08 +0900

    dark 모드 추가

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -3,8 +3,26 @@ import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181),);
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+
 void main() {
   runApp(MaterialApp(
+    darkTheme: ThemeData.dark().copyWith(
+      colorScheme: kDarkColorScheme,
+      cardTheme: const CardTheme().copyWith(
+        color: kDarkColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kDarkColorScheme.primaryContainer,
+          foregroundColor: kDarkColorScheme.onPrimaryContainer
+        ),
+      ),
+    ),
     theme: ThemeData().copyWith(
       colorScheme: kColorScheme,
       appBarTheme: const AppBarTheme().copyWith(
@@ -24,6 +42,7 @@ void main() {
        titleLarge: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: kColorScheme.onSecondaryContainer),
       )
     ),
+    themeMode: ThemeMode.dark,
     home: const Expenses(),
   ));
 }
