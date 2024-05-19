ab3f2bd97f775af09804c2dc135a4f08bf22d63b
gangbeomsu <kbs21c1987@gmail.com>
2024-05-19 15:11:42 +0900

    네비게이션을 이용해서 해당 페이지로 라우팅 구현

--- meals/lib/screens/categories.dart
+++ meals/lib/screens/categories.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          return MealsScreen(title: category.title, meals: filteredMeals);
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,17 +28,20 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('카테고리를 고르세요.'),
       ),
       body: GridView(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children:  [
-          for(final category in availableCategories)
-            CategoryGridItem(category: category),
-        ]
-      ),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: [
+            for (final category in availableCategories)
+              CategoryGridItem(
+                  category: category,
+                  onSelectCategory: () {
+                    _selectCategory(context, category);
+                  }),
+          ]),
     );
   }
 }
