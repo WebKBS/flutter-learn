769f67c12d35a1ca204e25a95c73d56ce149277e
gangbeomsu <kbs21c1987@gmail.com>
2024-05-25 20:48:46 +0900

    스위치 버튼 filter 기능 추가

--- meals/lib/screens/filters.dart
+++ meals/lib/screens/filters.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 // import 'package:meals/screens/taps.dart';
 // import 'package:meals/widgets/main_drawer.dart';
 
+enum Filter {
+  glutenFree,
+  lactoseFree,
+  vegetarian,
+  vegan,
+}
+
 class FiltersScreen extends StatefulWidget {
-  const FiltersScreen({super.key});
+  const FiltersScreen({super.key, required this.currentFilters});
+
+  final Map<Filter, bool> currentFilters;
 
   @override
   State<FiltersScreen> createState() => _FiltersScreenState();
@@ -13,8 +22,17 @@ class FiltersScreen extends StatefulWidget {
 class _FiltersScreenState extends State<FiltersScreen> {
   var _glutenFreeFilterSet = false;
   var _lactoseFreeFilterSet = false;
-  var _vegetarianFreeFilterSet = false;
-  var _veganFreeFilterSet = false;
+  var _vegetarianFilterSet = false;
+  var _veganFilterSet = false;
+
+  @override
+  void initState() {
+    _glutenFreeFilterSet = widget.currentFilters[Filter.glutenFree]!;
+    _lactoseFreeFilterSet = widget.currentFilters[Filter.lactoseFree]!;
+    _vegetarianFilterSet = widget.currentFilters[Filter.vegetarian]!;
+    _veganFilterSet = widget.currentFilters[Filter.vegan]!;
+    super.initState();
+  }
 
   Widget _buildSwitchListTile(String title, String description,
       bool currentValue, void Function(bool)? updateValue) {
@@ -41,68 +59,84 @@ class _FiltersScreenState extends State<FiltersScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('필터'),
-      ),
-      // drawer: MainDrawer(
-      //   onSelectScreen: (identifier) {
-      //     Navigator.of(context).pop();
-      //     if (identifier == 'meals') {
-      //       Navigator.of(context).pushReplacement(
-      //         MaterialPageRoute(
-      //           builder: (context) => const TabsScreen(),
-      //         ),
-      //       );
-      //     }
-      //   },
-      // ),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            _buildSwitchListTile(
-              '글루텐 프리',
-              '글루텐이 포함된 음식을 필터링합니다.',
-              _glutenFreeFilterSet,
-              (newValue) {
-                setState(() {
-                  _glutenFreeFilterSet = newValue;
-                });
-              },
-            ),
-            _buildSwitchListTile(
-              '락토스 프리',
-              '락토스가 포함된 음식을 필터링합니다.',
-              _lactoseFreeFilterSet,
-              (newValue) {
-                setState(() {
-                  _lactoseFreeFilterSet = newValue;
-                });
-              },
-            ),
-            _buildSwitchListTile(
-              '베지테리언',
-              '육류를 제외한 음식을 필터링합니다.',
-              _vegetarianFreeFilterSet,
-              (newValue) {
-                setState(() {
-                  _vegetarianFreeFilterSet = newValue;
-                });
-              },
-            ),
-            _buildSwitchListTile(
-              '비건',
-              '동물성 제품을 제외한 음식을 필터링합니다.',
-              _veganFreeFilterSet,
-              (newValue) {
-                setState(() {
-                  _veganFreeFilterSet = newValue;
-                });
-              },
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('필터'),
         ),
-      ),
-    );
+        // drawer: MainDrawer(
+        //   onSelectScreen: (identifier) {
+        //     Navigator.of(context).pop();
+        //     if (identifier == 'meals') {
+        //       Navigator.of(context).pushReplacement(
+        //         MaterialPageRoute(
+        //           builder: (context) => const TabsScreen(),
+        //         ),
+        //       );
+        //     }
+        //   },
+        // ),
+
+        body: PopScope(
+          canPop: false,
+          onPopInvoked: (bool didPop) {
+            if (didPop) return;
+            Navigator.of(context).pop({
+              Filter.glutenFree: _glutenFreeFilterSet,
+              Filter.lactoseFree: _lactoseFreeFilterSet,
+              Filter.vegetarian: _vegetarianFilterSet,
+              Filter.vegan: _veganFilterSet,
+            });
+          },
+          child: Column(
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    _buildSwitchListTile(
+                      '글루텐 프리',
+                      '글루텐이 포함된 음식을 필터링합니다.',
+                      _glutenFreeFilterSet,
+                      (newValue) {
+                        setState(() {
+                          _glutenFreeFilterSet = newValue;
+                        });
+                      },
+                    ),
+                    _buildSwitchListTile(
+                      '락토스 프리',
+                      '락토스가 포함된 음식을 필터링합니다.',
+                      _lactoseFreeFilterSet,
+                      (newValue) {
+                        setState(() {
+                          _lactoseFreeFilterSet = newValue;
+                        });
+                      },
+                    ),
+                    _buildSwitchListTile(
+                      '베지테리언',
+                      '육류를 제외한 음식을 필터링합니다.',
+                      _vegetarianFilterSet,
+                      (newValue) {
+                        setState(() {
+                          _vegetarianFilterSet = newValue;
+                        });
+                      },
+                    ),
+                    _buildSwitchListTile(
+                      '비건',
+                      '동물성 제품을 제외한 음식을 필터링합니다.',
+                      _veganFilterSet,
+                      (newValue) {
+                        setState(() {
+                          _veganFilterSet = newValue;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
--- meals/lib/screens/taps.dart
+++ meals/lib/screens/taps.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/main_drawer.dart';
+
+const kInitialFilters = {
+  Filter.glutenFree: false,
+  Filter.lactoseFree: false,
+  Filter.vegetarian: false,
+  Filter.vegan: false,
+};
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -15,6 +23,14 @@ class TabsScreen extends StatefulWidget {
 class _TabsScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
   final List<Meal> _favoriteMeals = [];
+
+  // 초기값을 false로 설정
+  Map<Filter, bool> _selectedFilters = {
+    Filter.glutenFree: false,
+    Filter.lactoseFree: false,
+    Filter.vegetarian: false,
+    Filter.vegan: false,
+  };
 
   void _showInfoMessage(String message) {
     ScaffoldMessenger.of(context).clearSnackBars();
@@ -47,21 +63,44 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
-  void _setScreen(String identifier) {
+  void _setScreen(String identifier) async {
     Navigator.of(context).pop();
     if (identifier == 'filters') {
-      Navigator.of(context).push(
+      final result = await Navigator.of(context).push<Map<Filter, bool>>(
         MaterialPageRoute(
-          builder: (context) => const FiltersScreen(),
+          builder: (context) => FiltersScreen(
+            currentFilters: _selectedFilters,
+          ),
         ),
       );
+
+      setState(() {
+        _selectedFilters = result ?? kInitialFilters;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    final availableMeals = dummyMeals.where((meal) {
+      if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+        return false;
+      }
+      if (_selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+        return false;
+      }
+      if (_selectedFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+        return false;
+      }
+      if (_selectedFilters[Filter.vegan]! && !meal.isVegan) {
+        return false;
+      }
+      return true;
+    }).toList();
+
     Widget activePage = CategoriesScreen(
       onToggleFavorite: _toggleMealFavoriteStatus,
+      availableMeals: availableMeals,
     );
 
     var activePageTitle = "카테고리";
