78f4a2e1655627c86ef4eb2a8bdd41a22db82181
gangbeomsu <kbs21c1987@gmail.com>
2024-05-25 19:46:20 +0900

    switch버튼 반복줄이기

--- meals/lib/screens/filters.dart
+++ meals/lib/screens/filters.dart
@@ -12,6 +12,31 @@ class FiltersScreen extends StatefulWidget {
 
 class _FiltersScreenState extends State<FiltersScreen> {
   var _glutenFreeFilterSet = false;
+  var _lactoseFreeFilterSet = false;
+  var _vegetarianFreeFilterSet = false;
+  var _veganFreeFilterSet = false;
+
+  Widget _buildSwitchListTile(String title, String description,
+      bool currentValue, void Function(bool)? updateValue) {
+    return SwitchListTile(
+      value: currentValue,
+      onChanged: updateValue,
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+      ),
+      subtitle: Text(
+        description,
+        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface,
+            ),
+      ),
+      activeColor: Theme.of(context).colorScheme.tertiary,
+      contentPadding: const EdgeInsets.only(left: 34, right: 22),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,28 +59,47 @@ class _FiltersScreenState extends State<FiltersScreen> {
       body: Center(
         child: Column(
           children: [
-            SwitchListTile(
-              value: _glutenFreeFilterSet,
-              onChanged: (isChecked) {
+            const SizedBox(height: 20),
+            _buildSwitchListTile(
+              '글루텐 프리',
+              '글루텐이 포함된 음식을 필터링합니다.',
+              _glutenFreeFilterSet,
+              (newValue) {
                 setState(() {
-                  _glutenFreeFilterSet = isChecked;
+                  _glutenFreeFilterSet = newValue;
                 });
               },
-              title: Text(
-                "Gluten-free",
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                      color: Theme.of(context).colorScheme.onSurface,
-                    ),
-              ),
-              subtitle: Text(
-                "Only include gluten-free meals.",
-                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: Theme.of(context).colorScheme.onSurface,
-                    ),
-              ),
-              activeColor: Theme.of(context).colorScheme.tertiary,
-              contentPadding: const EdgeInsets.only(left: 34, right: 22),
-            )
+            ),
+            _buildSwitchListTile(
+              '락토스 프리',
+              '락토스가 포함된 음식을 필터링합니다.',
+              _lactoseFreeFilterSet,
+              (newValue) {
+                setState(() {
+                  _lactoseFreeFilterSet = newValue;
+                });
+              },
+            ),
+            _buildSwitchListTile(
+              '베지테리언',
+              '육류를 제외한 음식을 필터링합니다.',
+              _vegetarianFreeFilterSet,
+              (newValue) {
+                setState(() {
+                  _vegetarianFreeFilterSet = newValue;
+                });
+              },
+            ),
+            _buildSwitchListTile(
+              '비건',
+              '동물성 제품을 제외한 음식을 필터링합니다.',
+              _veganFreeFilterSet,
+              (newValue) {
+                setState(() {
+                  _veganFreeFilterSet = newValue;
+                });
+              },
+            ),
           ],
         ),
       ),
