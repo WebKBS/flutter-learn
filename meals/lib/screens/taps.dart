d6c4f291866f2a30c0fcd8c1403bde8af348da5a
gangbeomsu <kbs21c1987@gmail.com>
2024-05-26 14:15:32 +0900

    tab 조건문 provider로 이동 리팩토링

--- meals/lib/screens/taps.dart
+++ meals/lib/screens/taps.dart
@@ -45,25 +45,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
-
-    final activeFilters = ref.watch(filtersProvider);
-
-    final availableMeals = meals.where((meal) {
-      if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
-        return false;
-      }
-      if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-        return false;
-      }
-      if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-        return false;
-      }
-      if (activeFilters[Filter.vegan]! && !meal.isVegan) {
-        return false;
-      }
-      return true;
-    }).toList();
+    final availableMeals = ref.watch(filteredMealsProvider);
 
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
@@ -72,9 +54,9 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
     var activePageTitle = "카테고리";
 
     if (_selectedPageIndex == 1) {
-      final favorqqiteMeasl = ref.watch(favoriteMealsProvider);
+      final favoriteMeal = ref.watch(favoriteMealsProvider);
       activePage = MealsScreen(
-        meals: favorqqiteMeasl,
+        meals: favoriteMeal,
       );
       activePageTitle = "즐겨찾기";
     }
