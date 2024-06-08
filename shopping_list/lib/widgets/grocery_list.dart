71b0bc682d03b20a5bb12a60dd9c0eb1e4919171
gangbeomsu <kbs21c1987@gmail.com>
2024-06-08 15:46:17 +0900

    new Item 페이지 추가

---
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const NewItem(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Grocery List'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () => _addItem(),
+          )],
+      ),
+      body: ListView.builder(
+        itemCount: groceryItems.length,
+        itemBuilder: (context, index) {
+          final item = groceryItems[index];
+          return ListTile(
+            title: Text(item.name),
+            subtitle: Text('Quantity: ${item.quantity}'),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: item.category.color,
+            ),
+            trailing: Text(
+              item.category.title,
+              style: TextStyle(color: item.category.color),
+            ),
+          );
+        },
+      )
+    );
+  }
+}
