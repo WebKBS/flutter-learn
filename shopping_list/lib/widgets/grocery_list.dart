9d0d9c14403edf82764b5a587cfbab377e073bc5
gangbeomsu <kbs21c1987@gmail.com>
2024-06-08 17:06:05 +0900

    삭제 기능 및 데이터 추가 기능

--- shopping_list/lib/widgets/grocery_list.dart
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
 class GroceryList extends StatefulWidget {
@@ -10,44 +11,78 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  void _addItem() {
-    Navigator.of(context).push(
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (context) => const NewItem(),
       ),
     );
+
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  void _removeItem(String id) {
+    setState(() {
+      _groceryItems.removeWhere((item) => item.id == id);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Grocery List'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () => _addItem(),
-          )],
-      ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (context, index) {
-          final item = groceryItems[index];
-          return ListTile(
-            title: Text(item.name),
-            subtitle: Text('Quantity: ${item.quantity}'),
-            leading: Container(
-              width: 24,
-              height: 24,
-              color: item.category.color,
-            ),
-            trailing: Text(
-              item.category.title,
-              style: TextStyle(color: item.category.color),
-            ),
-          );
-        },
-      )
+    Widget content = const Center(
+      child: Text("데이터가 없습니다."),
     );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (context, index) {
+          final item = _groceryItems[index];
+
+          return Dismissible(
+              onDismissed: (_) => _removeItem(item.id),
+              background: Container(
+                color: Colors.red,
+                alignment: Alignment.centerRight,
+                padding: const EdgeInsets.only(right: 16),
+                child: const Icon(Icons.delete, color: Colors.white, size: 24),
+              ),
+              key: ValueKey(item.id),
+              child: ListTile(
+                title: Text(item.name),
+                subtitle: Text('Quantity: ${item.quantity}'),
+                leading: Container(
+                  width: 24,
+                  height: 24,
+                  color: item.category.color,
+                ),
+                trailing: Text(
+                  item.category.title,
+                  style: TextStyle(color: item.category.color),
+                ),
+              ));
+        },
+      );
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Grocery List'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () => _addItem(),
+            )
+          ],
+        ),
+        body: content);
   }
 }
