6e2c6b16f5c07322dcb7d1732a63dc0a6d308552
gangbeomsu <kbs21c1987@gmail.com>
2024-06-15 16:34:48 +0900

    detail 기능 구현

---
+++ native_device/lib/screens/places_detail.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:native_device/place/place.dart';
+
+class PlacesDetailScreen extends StatelessWidget {
+  const PlacesDetailScreen({super.key, required this.place});
+
+  final Place place;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(place.title),
+      ),
+      body: Center(
+        child: Text(
+          place.title,
+          style: Theme.of(context)
+              .textTheme
+              .bodyLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onSurface),
+        ),
+      ),
+    );
+  }
+}
--- native_device/lib/widgets/Places_list.dart
+++ native_device/lib/widgets/Places_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:native_device/place/place.dart';
+import 'package:native_device/screens/places_detail.dart';
 
 class PlacesList extends StatelessWidget {
   const PlacesList({super.key, required this.places});
@@ -24,14 +25,20 @@ class PlacesList extends StatelessWidget {
       itemBuilder: (context, index) {
         final place = places[index];
         return ListTile(
-          title: Text(
-            place.title,
-            style: Theme.of(context)
-                .textTheme
-                .titleMedium!
-                .copyWith(color: Theme.of(context).colorScheme.onSurface),
-          ),
-        );
+            title: Text(
+              place.title,
+              style: Theme.of(context)
+                  .textTheme
+                  .titleMedium!
+                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
+            ),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => PlacesDetailScreen(place: place),
+                ),
+              );
+            });
       },
     );
   }
